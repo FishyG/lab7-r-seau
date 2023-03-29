f18584cab49e7d7675288f163aeefac34fbcd341
fishy <fishy@fishynetworks.com>
2023-03-29 09:46:50 -0400

    Added all the files

---
+++ cgi-bin/test.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+
+print "Content-type: text/html\n\n";
+print "<h1>Jessy is sleeping</h1>\n";
+print "Hello CGI (Perl)\n";
+print "<p>&#128293;</p>\n";
---
+++ cgi-bin/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+print("Content-type: text/html\n")
+print("<html>\n<body>")
+print("<div style=\"width: 100%; font-size: 40px; font-weight: bold; text-align: center;\">")
+print("<h1>Jessy was here</h1>")
+print("CGI Script Test Page (Python)")
+print('<p style="font-size:200px">')
+print('&#128013\n</p>')
+print("</div>")
+print("</body>\n</html>")
